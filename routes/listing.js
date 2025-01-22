e5ab4d2cd679bda7f37dc95b1ffcf33ad391fa88
Kanishk Sharma <sharmakanishk413@gmail.com>
2025-01-22 21:41:47 +0530

    Fix file path issues for deployment on Render

--- routes/listing.js
+++ routes/listing.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
-const Listing = require("C:/Users/sharm/OneDrive/Pictures/air bnb/models/listing.js");
+const Listing = require("../models/listing.js");
 const {isLoggedIn, isOwner,validateListing} = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 const multer  = require('multer');
